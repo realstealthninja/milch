b2423383c305f89144e7a9e4f8af62ac89cc0d02
realstealthninja <realstealthninja@gmail.com>
2023-06-30 19:37:16 +0530

    fix: Add an example for a simple window

---
+++ examples/simple_window.cpp
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <milch.h>
+using namespace milch;
+
+int main() {
+    window win(400,200, "fuck my window");
+    win.show_window();
+    sleep(200);
+    return 0;
+}
