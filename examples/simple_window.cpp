6d6f687d48136b951d567c9044b2206399d293a5
realstealthninja <realstealthninja@gmail.com>
2023-06-30 19:45:59 +0530

    docs: add documentation to simple_window.cpp

--- examples/simple_window.cpp
+++ examples/simple_window.cpp
@@ -1,10 +1,26 @@
-#include <unistd.h>
-#include <milch.h>
+/**
+ * @file simple_window.cpp
+ * @brief A simple window using milch!
+ * @details
+ * This file contains a simple window made using libmilch
+ * hopefully this shows you the potential of using milch
+ *
+ * @copyright This project is licensed under the MIT license
+ * @author [stealthninja](https://github.com/realstealthninja)
+ */
+
+#include <unistd.h> /// for sleep
+#include <milch.h> /// for window and show_window function
+
 using namespace milch;
 
+/**
+ * @brief Main function
+ * @returns 0 on success
+ */
 int main() {
     window win(400,200, "my window");
     win.show_window();
-    sleep(200);
+    sleep(200); // waiting long enough to see window
     return 0;
 }
