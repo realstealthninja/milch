9137464c0da03c93c6d3077f82876b7372848103
realstealthninja <realstealthninja@gmail.com>
2023-08-05 15:32:46 +0530

    feat: create error function

---
+++ include/error.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file error.hpp
+ * @brief Errors are defined here
+ * @description
+ * All errors and error handler is defined here
+ * Used internally
+ */
+#ifndef MILCH_ERROR_HPP
+#define MILCH_ERROR_HPP
+
+/**
+ * @brief namespace containing all functions of milch
+ * @namespace error
+ */
+ namespace milch::error {
+
+
+} // namespace error
+#endif //MILCH_ERROR_HPP
