5e95545d097925c3cf281bea22ddcbcd2f175d57
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-28 10:06:15 +0530

    feat: add a simple paint app

---
+++ examples/milch_paint/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file main.cpp
+ * @brief A simple paint using milch!
+ * @details
+ * This file contains a simple paint application made using lib milch
+ *
+ * @copyright This project is licensed under the MIT license
+ * @author [stealthninja](https://github.com/realstealthninja)
+ */
+
+
+#include <unistd.h> /// for sleep
+#include <milch/milch.h> /// for window and show_window function
+
+using namespace milch;
+
+/**
+ * @brief Main function
+ * @returns 0 on success
+ */
+int main() {
+    milch::Window win(400,200, "my window");
+    win.show_window();
+
+    while(true) {
+        shapes::point mos_pos = win.get_mouse_position();
+
+        win.draw_rectangle(shapes::point(mos_pos.x-1, mos_pos.y-1), shapes::point(mos_pos.x+1, mos_pos.y+1), 1, true);
+    }
+
+}
