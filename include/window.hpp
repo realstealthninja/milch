2d9b6e21a6c07cd70bc38b33d712ead52b2238b3
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-01 09:41:44 +0530

    lint: window

--- include/window.hpp
+++ include/window.hpp
@@ -11,11 +11,11 @@
 #ifndef MILCH_WINDOW_HPP
 #define MILCH_WINDOW_HPP
 
-#include <X11/Xlib.h> /// For XLib functions
-#include <string> /// For std::string
-#include <iostream> /// For I/O operations
 #include "color.hpp"  /// For color
 #include "shapes.hpp" /// For shapes::point
+#include <X11/Xlib.h> /// For XLib functions
+#include <iostream>   /// For I/O operations
+#include <string>     /// For std::string
 
 typedef Window XWindow;
 
@@ -25,131 +25,128 @@ typedef Window XWindow;
  */
 namespace milch {
 
-    /**
-     * @brief class contains functions relating to windows
-     * @class window
-     */
-    class Window {
+/**
+ * @brief class contains functions relating to windows
+ * @class window
+ */
+class Window {
 
-    private:
-        Display *display = XOpenDisplay(nullptr); ///< display struct from XLib
-        GC gc{}; ///< Graphics context of window / display
-        XWindow win {}; ///< window struct from XLib
+private:
+  Display *display = XOpenDisplay(nullptr); ///< display struct from XLib
+  GC gc{};       ///< Graphics context of window / display
+  XWindow win{}; ///< window struct from XLib
 
-        Colormap color_map; ///< color map of the given display
+  Colormap color_map; ///< color map of the given display
 
-    public:
-        size_t width{}; ///< window width
-        size_t height{}; ///< window height
-        std::string title; ///< window's title
+public:
+  size_t width{};    ///< window width
+  size_t height{};   ///< window height
+  std::string title; ///< window's title
 
+  /**
+   * @brief base window constructor creates a window using XLib
+   * @param width of the window
+   * @param height of the window
+   */
+  Window(size_t width, size_t height);
 
+  /**
+   * @brief A simple constructor for easier development
+   * @param width of the window
+   * @param height of the window
+   * @param title of the window
+   */
+  Window(size_t width, size_t height, const std::string &title,
+         const milch::color &background_color = milch::color(255, 255, 255),
+         const milch::color &foreground_color = milch::color(1, 1, 1));
 
-        /**
-         * @brief base window constructor creates a window using XLib
-         * @param width of the window
-         * @param height of the window
-         */
-        Window(size_t width, size_t height);
+  /**
+   * @brief copy constructor of window
+   */
+  Window(Window &w);
 
-        /**
-         * @brief A simple constructor for easier development
-         * @param width of the window
-         * @param height of the window
-         * @param title of the window
-         */
-        Window(size_t width,
-               size_t height,
-               const std::string &title,
-               const milch::color &background_color = milch::color(255,255,255),
-               const milch::color &foreground_color = milch::color(1,1,1));
+  /**
+   * @brief move constructor of window
+   */
+  Window(Window &&w) = default;
 
-        /**
-         * @brief copy constructor of window
-         */
-        Window(Window &w);
+  /**
+   * @brief Makes the window visible
+   * @returns void
+   */
+  void show_window() const;
 
-        /**
-         * @brief move constructor of window
-         */
-        Window(Window&& w) = default;
+  /**
+   * @brief Set the background colour of the window
+   * @param hex_color color of the background in hex
+   * @returns void
+   */
+  void set_background_color(const milch::color &color) const;
 
-        /**
-         * @brief Makes the window visible
-         * @returns void
-         */
-        void show_window() const;
+  /**
+   * @brief Set the background colour of the window
+   * @param hex_color color of the background in hex
+   * @returns void
+   */
+  void set_foreground_color(const milch::color &color) const;
 
-        /**
-         * @brief Set the background colour of the window
-         * @param hex_color color of the background in hex
-         * @returns void
-         */
-        void set_background_color(const milch::color& color) const;
+  /**
+   * @brief Internal Error handler
+   * @param d display causing the error
+   * @param error XErrorEvent of the error
+   * @return 0 and exit the program if an erorr occurs
+   */
+  static int error_handler(Display *d, XErrorEvent *error) {
+    char buf[256];
+    XGetErrorText(d, error->error_code, buf, 256);
 
-        /**
-         * @brief Set the background colour of the window
-         * @param hex_color color of the background in hex
-         * @returns void
-         */
-        void set_foreground_color(const milch::color& color) const;
+    std::cerr << "\033[1;31mERROR\033[0m XLIB " << buf << "\n";
+    std::cerr.flush();
+    return 0;
+  }
 
-        /**
-         * @brief Internal Error handler
-         * @param d display causing the error
-         * @param error XErrorEvent of the error
-         * @return 0 and exit the program if an erorr occurs
-         */
-        static int error_handler(Display *d, XErrorEvent *error) {
-            char* buf;
-            XGetErrorText(d, error->error_code, buf, 256);
+  // Drawing section
+  void draw_line(shapes::point a, shapes::point b, int width_of_line = 1) const;
 
-            std::cerr << "\033[1;31mERROR\033[0m XLIB " << buf << "\n";
-            std::cerr.flush();
-            return 0;
-        }
+  void draw_rectangle(shapes::point a, shapes::point b, int width_of_line = 1,
+                      bool fill = false) const;
+  // end of drawing section
 
-        // Drawing section
-        void draw_line(shapes::point a, shapes::point b, int width_of_line = 1) const;
+  void draw_circle(shapes::point center, float radius);
 
-        void draw_rectangle(shapes::point a, shapes::point b, int width_of_line = 1, bool fill = false) const;
-        // end of drawing section
+  /**
+   * @brief draws everything to the screen
+   *
+   */
+  void draw();
 
-        void draw_circle(shapes::point center, float radius);
+  shapes::point get_mouse_position();
 
-        /**
-         * @brief draws everything to the screen
-         * 
-         */
-        void draw();
+  /**
+   * @breif conversion of window to display
+   * @return XLib Display*
+   */
+  explicit operator Display *() const;
 
-        shapes::point get_mouse_position();
+  /**
+   * @breif conversion of window to Window
+   * @return XLib Window
+   */
+  explicit operator XWindow() const;
 
-        /**
-         * @breif conversion of window to display
-         * @return XLib Display*
-         */
-        explicit operator Display *() const;
-
-        /**
-         * @breif conversion of window to Window
-         * @return XLib Window
-         */
-        explicit operator XWindow() const;
-
-        /**
-         * @breif conversion of window to graphics context
-         * @return XLib Graphics context
-         */
-        explicit operator GC() const;
-        /**
-         * @brief window deconstruction
-         * @details
-         * deletes all the pointers and alleviates memory
-         */
-        ~Window();
-    };
+  /**
+   * @breif conversion of window to graphics context
+   * @return XLib Graphics context
+   */
+  explicit operator GC() const;
+  /**
+   * @brief window deconstruction
+   * @details
+   * deletes all the pointers and alleviates memory
+   */
+  ~Window();
+};
 
 } // namespace milch
 
-#endif //MILCH_WINDOW_HPP
+#endif // MILCH_WINDOW_HPP
